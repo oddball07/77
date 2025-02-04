ce7f14e1ef695c0624e63a1ef23f9282b336250a
oddball07 <160886911+oddball07@users.noreply.github.com>
2025-02-05 01:11:41 +0530

    with backend version and some image changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import Loading from "./loading";
 import "./home.css";
 import Nav from "./components/Nav";
+import Image from "next/image";
+import spiderImage from "./assets/new spider.jpg";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -21,7 +23,13 @@ export default function Home() {
     <>
       <div className="homepage">
         <Nav />
-        <div className="details"></div>
+        <div className="details">
+          <Image
+            src={spiderImage}
+            style={{ width: "100%", height: "100%" }}
+            alt="spider image"
+          ></Image>
+        </div>
       </div>
     </>
   );
